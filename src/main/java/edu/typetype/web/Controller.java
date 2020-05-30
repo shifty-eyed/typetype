940887591ae85574ffe5866045379acc8e542316
rrr <rrr@DESKTOP-P7POH37.home>
2020-05-29 22:43:51 -0400

    java part loading and saving progress

--- src/main/java/edu/typetype/web/Controller.java
+++ src/main/java/edu/typetype/web/Controller.java
@@ -1,12 +1,21 @@
 package edu.typetype.web;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.typetype.service.FingerSignalingDevice;
 
@@ -16,9 +25,35 @@ public class Controller {
 	@Autowired
 	FingerSignalingDevice device;
 	
+	@Value("${typetype.data.dir}")
+	private String dataDir;
+	
+	private static final ObjectMapper json = new ObjectMapper(); 
+	
+	private static final String LESSON_FILE = "lesson.json"; 
+	private static final String COMPLETED_FILE_PREFIX = "completed.json"; 
+	
 	@RequestMapping("/getLessons")
 	public String getLessons() throws IOException {
-		return IOUtils.resourceToString("/lessons.json", Charset.forName("UTF-8"));
+		try(InputStream in = new FileInputStream(dataDir+"/"+LESSON_FILE)) {
+			return IOUtils.toString(in, Charset.forName("UTF-8"));
+		}
+	}
+	
+	@RequestMapping("/getCompletedLessons")
+	public String[] getCompletedLessons() throws IOException {
+		try(InputStream in = new FileInputStream(dataDir+COMPLETED_FILE_PREFIX)) {
+			return json.readValue(IOUtils.toString(in, Charset.forName("UTF-8")), String[].class);
+		}
+	}
+	
+	@RequestMapping("/markLessonCompleted")
+	public void markLessonCompleted(String lessonId) throws IOException {
+		List<String> lessons = Arrays.asList(getCompletedLessons());
+		lessons.add(lessonId);
+		try(OutputStream out = new FileOutputStream(dataDir+COMPLETED_FILE_PREFIX)) {
+			IOUtils.write(json.writeValueAsString(lessons.toArray()), out, Charset.forName("UTF-8"));
+		}
 	}
 	
 	@RequestMapping("/fingerSignal")
@@ -26,6 +61,4 @@ public class Controller {
 		device.signal(key);
 	}
 	
-
-
 }
